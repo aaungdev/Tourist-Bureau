27893cfc500b2b31665c298d6d1ce2a4382315eb
aaungdev <dhev.code@gmail.com>
2024-05-12 00:06:57 -0400

    added tabs, update footer

--- scripts/index.js
+++ scripts/index.js
@@ -21,3 +21,19 @@ function showSlides(n) {
     }
     slides[slideIndex].style.opacity = '1';
 }
+
+function openTab(evt, tabName) {
+    let i, tabContent, tabLinks;
+    tabContent = document.getElementsByClassName("tourCard");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+    tabLinks = document.getElementsByClassName("tabLinks");
+    for (i = 0; i < tabLinks.length; i++) {
+        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
