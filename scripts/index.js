ecde1b5db76d378b852d32fe111480ec5c7d4232
aaungdev <dhev.code@gmail.com>
2024-05-12 01:03:07 -0400

    debug javascript

--- scripts/index.js
+++ scripts/index.js
@@ -22,18 +22,37 @@ function showSlides(n) {
     slides[slideIndex].style.opacity = '1';
 }
 
-function openTab(evt, tabName) {
-    let i, tabContent, tabLinks;
-    tabContent = document.getElementsByClassName("tourCard");
-    for (i = 0; i < tabContent.length; i++) {
-        tabContent[i].style.display = "none";
+window.onload = function() {
+    function openTab(evt, tabName) {
+        // Hide all tab content first
+        var i, tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";  // Hide each content
+        }
+
+        // Remove the "active" class from all tabs
+        var tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+
+        // Show the specific tabs related to the clicked tab link
+        var activeContent = document.querySelectorAll('#' + tabName + ' .tabcontent');  // Select all child divs
+        for (i = 0; i < activeContent.length; i++) {
+            activeContent[i].style.display = "block";  // Display each child content
+        }
+
+        // Add "active" class to the clicked tab link
+        evt.currentTarget.className += " active";
     }
-    tabLinks = document.getElementsByClassName("tabLinks");
-    for (i = 0; i < tabLinks.length; i++) {
-        tabLinks[i].className = tabLinks[i].className.replace(" active", "");
+
+    // Set the default tab to be open
+    // Replace 'ActiveTours' with the ID of the tab you want open by default
+    document.getElementById('ActiveTours').style.display = 'block'; 
+    var defaultTab = document.querySelector('.tablinks');
+    if (defaultTab) {
+        defaultTab.click(); // Simulate a click on the default tab
     }
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
 }
 
 
